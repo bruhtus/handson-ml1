62c84e1038ca9bca50f075ffa08971fd4503a8a6
bruhtus <hibruhtus@gmail.com>
2021-02-03 06:54:01 +0700

    Display one of mnist data with and without axis

---
+++ chap-3/classification.py
@@ -0,0 +1,36 @@
+import numpy as np
+import os
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from fire import Fire
+from sklearn.datasets import fetch_openml
+
+def main():
+    mnist = fetch_openml('mnist_784', version=1, cache=True)
+    mnist.target = mnist.target.astype(np.int8)
+    sort_by_target(mnist)
+    X, y = mnist['data'], mnist['target']
+    some_digit = X[36000]
+    some_digit_image = some_digit.reshape(28, 28)
+    plt.imshow(some_digit_image, cmap=mpl.cm.binary, interpolation='nearest')
+    plt.axis('off')
+    save_fig('some_digit_mnist')
+
+def sort_by_target(mnist):
+    reorder_train = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[:60000])]))[:, 1]
+    reorder_test = np.array(sorted([(target, 1) for i, target in enumerate(mnist.target[:60000])]))[:, 1]
+    mnist.data[:60000] = mnist.data[reorder_train]
+    mnist.target[:60000] = mnist.target[reorder_train]
+    mnist.data[:60000] = mnist.data[reorder_test + 60000]
+    mnist.target[:60000] = mnist.target[reorder_test + 60000]
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join('.', fig_id + ".png")
+    print('Saving figure', fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+
+if __name__ == '__main__':
+    Fire(main)
