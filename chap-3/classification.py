3fda43be107d79003cd1e5a976821ae3d049c939
bruhtus <hibruhtus@gmail.com>
2021-02-03 13:26:10 +0700

    Dataset mnist_784 with version 1 not found error

--- chap-3/classification.py
+++ chap-3/classification.py
@@ -13,16 +13,15 @@ def main():
     sort_by_target(mnist)
     X, y = mnist['data'], mnist['target']
     some_digit = X[36000]
-    X_test, y_test = X[60000:], y[60000:]
+    X_train, y_train, X_test, y_test = X[:60000], y[:60000], X[60000:], y[60000:]
     shuffle_index = np.random.permutation(60000)
-    X_train, y_train = X[shuffle_index], y[shuffle_index]
+    X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
 
     y_train_5 = (y_train == 5)
     y_test_5 = (y_test == 5)
 
-    sgd_clf = SGDClassifier(random_state=42)
+    sgd_clf = SGDClassifier(max_iter=5, tol=np.infty, random_state=42)
     sgd_clf.fit(X_train, y_train_5)
-    print(sgd_clf.predict([some_digit]))
 
 def sort_by_target(mnist):
     reorder_train = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[:60000])]))[:, 1]
